c231cdef9e718072944e3ab3c3a991746189dfe4
Sander van den Berg <svdberg@me.com>
2012-08-14 09:33:53 +0200

    changed the home login screen.

--- Settings/StaticFiles.hs
+++ Settings/StaticFiles.hs
@@ -15,4 +15,5 @@ staticSite = if development then Static.staticDevel staticDir
 --   giving you compile-time verification that referenced files exist.
 --   Warning: any files added to your static directory during run-time can't be
 --   accessed this way. You'll have to use their FilePath or URL to access them.
-$(staticFiles Settings.staticDir)
+$(publicFiles Settings.staticDir)
+-- $(staticFiles Settings.staticDir)
