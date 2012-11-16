ff99fa091e3908a9eb876f3fbaa62e41e21c6ea0
Sander van den Berg <svdberg@mac.com>
2012-11-16 13:58:41 +0100

    Changed namings

--- Handler/Feedings.hs
+++ Handler/Feedings.hs
@@ -7,36 +7,35 @@ import Yesod.Auth
 
 {- This module contains the Feeding resource -}
 
-getFeedingsR :: FeedingId -> Handler RepJson
-getFeedingsR fid = runDB (get404 fid) >>= jsonToRepJson . Entity fid
+getFeedingR :: FeedingId -> Handler RepJson
+getFeedingR fid = runDB (get404 fid) >>= jsonToRepJson . Entity fid
 
-deleteFeedingsR :: FeedingId -> Handler ()
-deleteFeedingsR fid = do --incomming Id is Base64, mongo expects Base16
+deleteFeedingR :: FeedingId -> Handler ()
+deleteFeedingR fid = do --incomming Id is Base64, mongo expects Base16
                   runDB $ delete fid
                   sendResponseStatus status200 ()
 
-putFeedingsR :: FeedingId -> Handler ()
-putFeedingsR fid =  do --incomming Id is Base64, mongo expects Base16
+putFeedingR :: FeedingId -> Handler ()
+putFeedingR fid =  do --incomming Id is Base64, mongo expects Base16
            feeding <- parseJsonBody_
            runDB $ replace fid feeding
            sendResponseStatus status200 ()
 
-postFeedingsR :: FeedingId -> Handler RepJson
-postFeedingsR feedingId = do
+postFeedingsR :: Handler RepJson
+postFeedingsR  = do
       muser <- maybeAuth
       parsedFeeding <- parseJsonBody_ --get content as JSON
       fid <- runDB $ insert parsedFeeding --store in mongo
       let userId = getUserId muser
       runDB $ update fid [ FeedingUserId =. userId ] --link the feeding to the user
-      sendResponseCreated $ FeedingsR fid --return the id
+      sendResponseCreated $ FeedingR fid --return the id
 
 getUserId :: Maybe (Entity t) -> Key (PersistEntityBackend t) t
 getUserId userEntity = case userEntity of
   Just (Entity k s ) -> k
 
-
-getFeedingR :: Handler RepJson
-getFeedingR = do
+getFeedingsR :: Handler RepJson
+getFeedingsR = do
   muser <- maybeAuth
   runDB (selectList [FeedingUserId ==. (getUserId muser)] [Desc FeedingDate, Desc FeedingTime]) >>= jsonToRepJson . asFeedingEntities
     where
--- Handler/Home.hs
+++ Handler/Home.hs
@@ -2,6 +2,7 @@
 module Handler.Home where
 
 import Import
+import Yesod.Auth
 
 -- This is a handler function for the GET request method on the HomeR
 -- resource pattern. All of your resource patterns are defined in
@@ -10,6 +11,7 @@ import Import
 -- The majority of the code you will write in Yesod lives in these handler
 -- functions. You can spread them across multiple files if you are so
 -- inclined, or create a single monolithic file.
+{-
 getHomeR :: Handler RepHtml
 getHomeR = do
     (formWidget, formEnctype) <- generateFormPost sampleForm
@@ -19,7 +21,16 @@ getHomeR = do
         aDomId <- lift newIdent
         setTitle "Welcome To Yesod!"
         $(widgetFile "homepage")
+-}
 
+getHomeR :: Handler RepHtml
+getHomeR = do
+           mauth <- maybeAuth
+           defaultLayout $ do
+             setTitle "Welcome To MilkMachine!"
+             $(widgetFile "homepage")
+
+{-
 postHomeR :: Handler RepHtml
 postHomeR = do
     ((result, formWidget), formEnctype) <- runFormPost sampleForm
@@ -37,3 +48,4 @@ sampleForm :: Form (FileInfo, Text)
 sampleForm = renderDivs $ (,)
     <$> fileAFormReq "Choose a file"
     <*> areq textField "What's on the file?" Nothing
+-}
